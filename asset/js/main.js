aeb4865a72c811677ddecc4448d55da29e6d9993
uwu-nish <anishforstudy@gmail.com>
2025-08-05 09:15:45 +0545

    pagination in blogs added

--- asset/js/main.js
+++ asset/js/main.js
@@ -35,18 +35,18 @@ function openWhatsapp() {
     window.open(url,"_blank");
 }
 
+let currentPage = 1;
 const apiURL = "https://dev.to/api/articles?page=1";
+let allPosts=[];
+const POSTS_PER_PAGE = 5;
 
-async function fetchPosts() {
-   console.log("Before fetch");
+async function fetchAllPosts() {
     try {
-        console.log("Before fetch")
         const res = await fetch(apiURL);
-        console.log("After fetch")
         if(!res.ok) {
             throw new Error(`Api error: ${res.status}`);
         }
-        return await res.json();
+        allPosts = await res.json();
     } catch (error) {
         console.error(error);
         return [];
@@ -66,7 +66,7 @@ function renderPosts(posts) {
         element.className = "row justify-content-center m-3";
         element.innerHTML = `
         <div class="col-md-8 mx-auto">
-            <div class="blog-post">
+            <div class="blog-post border-bottom rounded">
                 <a target="_blank" href="${post.url}">
                     <img src="${post.social_image}" class="img-fluid" alt=""/>
                     <h2>${post.title}</h2>
@@ -78,7 +78,47 @@ function renderPosts(posts) {
     })
 }
 
+function renderPagination() {
+    const ul = document.getElementById("pagination");
+    ul.innerHTML = '';
+    const pageCount = Math.ceil(allPosts.length / POSTS_PER_PAGE);
+
+    // Previous button
+    const prevLi = document.createElement("li");
+    prevLi.className = `page-item ${currentPage === 1 ? "disabled":""}`;
+    prevLi.innerHTML = `<button class="page-link" ${currentPage===1} ? 'tabindex="-1" aria-disabled="true"' : "">&laquo; Prev</button>`;
+    prevLi.onclick = () => changePage(currentPage -1);
+    ul.appendChild(prevLi);
+
+    for(let p = 1; p<=pageCount; p++) {
+        const li = document.createElement("li");
+        li.className = `page-item ${p === currentPage ? 'active':''}`;
+        li.innerHTML = `<button class="page-link">${p}</button>`;
+        li.onclick = () => changePage(p);
+        ul.appendChild(li);
+    }
+
+    // Next button
+    const nextLi = document.createElement("li");
+    nextLi.className = `page-item ${currentPage === 1 ? "disabled":""}`;
+    nextLi.innerHTML = `<button class="page-link" ${currentPage===pageCount} ? 'tabindex="-1" aria-disabled="true"' : "">Next &raquo;</button>`;
+    nextLi.onclick = () => changePage(currentPage+1);
+    ul.appendChild(nextLi);
+}
+
+function changePage(page) {
+    if(page < 1 || page > Math.ceil(allPosts.length/POSTS_PER_PAGE)) return;
+    currentPage = page;
+    const start = (page -1)*POSTS_PER_PAGE;
+    const slicedBlogs = allPosts.slice(start,start + POSTS_PER_PAGE);
+    renderPosts(slicedBlogs);
+    renderPagination();
+
+    document.getElementById("latest-blog").scrollIntoView({behavior:"smooth", block:"start"});
+}
+
 document.addEventListener("DOMContentLoaded",async() => {
-    const posts = await fetchPosts();
-    renderPosts(posts);
-})
+    await fetchAllPosts();
+    changePage(1);
+    // renderPosts(posts);
+});
